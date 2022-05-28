08b4e0276610c97e96ddc7e08130a06a4fcdef3c
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2022-05-28 20:54:29 +0900

    사람 수 세기 앱

--- app/src/main/java/com/yrcho/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/yrcho/myfirstapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.yrcho.myfirstapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+        btnClickMe.setOnClickListener{
+            timesClicked += 1
+            tvMyTextView.text = timesClicked.toString()
+            Toast.makeText(this, "Hey Ray!", Toast.LENGTH_LONG).show() // Toast 표시
+        }
     }
 }
